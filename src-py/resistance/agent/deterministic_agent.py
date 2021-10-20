fbc8f6ecaf5587093dea7bd3aa48eaedd89f4f59
RunSeven <5661180+RunSeven@users.noreply.github.com>
2021-10-20 09:51:11 +0800

    deterministic logging removed

--- src-py/resistance/agent/deterministic_agent.py
+++ src-py/resistance/agent/deterministic_agent.py
@@ -12,7 +12,6 @@ Gameplay:
                 Supports spies in votes more as
 """
 
-import logging
 import random
 
 from agent import Agent
@@ -55,24 +54,19 @@ class DeterministicAgent(Agent):
         self.missions_failed = 0
         self.current_round = 0
 
-        #
-        if isinstance(genetics, AgentGenetics):
-            logging.debug("AGENT: {} USING THRESHOLDS | DISTRUST {} | VOTE {} | BETAYAL {}".format(self.name, genetics.distrust, genetics.vote, genetics.betray))
+        # Set values on how the agent reacts to events
+        if isinstance(genetics, AgentGenetics):            
             self.genetics = genetics
         else:
-            logging.debug("AGENT: {} USING DEFAULT AGENT THRESHOLD".format(self.name))
             self.genetics = AgentGenetics()
 
         if isinstance(penalties, AgentPenalties):
-            logging.debug("AGENT: {} USING PENALTIES | FAILED MISSION {} | PROPOSE FAILED {} | ABORT MISSION {} | VOTE FOR SUSPECT {}".format(self.name, penalties.failed_mission, penalties.p_failed_mission, penalties.vote_fail, penalties.vote_spy))
             self.penalties = penalties
         else:
-            logging.debug("AGENT: {} USING DEFAULT AGENT PENALTIES".format(self.name))
             self.penalties = AgentPenalties()
 
         # Spy Variables
         self.target_resistance = list()
-
 
 
     def new_game(self, number_of_players, player_number, spies):
@@ -85,7 +79,6 @@ class DeterministicAgent(Agent):
 
         # Set Play Status
         self.spy = self.player_number in spies
-        logging.debug("{} ({}) IS A SPY {}".format(self.player_number, self.__class__.__name__, self.spy))
 
         trust_level = self._calculate_initial_spy_probability()
         self.agent_assessments = {player: AgentPredisposition(player)
@@ -155,7 +148,6 @@ class DeterministicAgent(Agent):
 
             # If self is burnt default to fail the vote
             if self.player_number in self._get_burnt_spies():
-                logging.debug("{} IS VOTING AGAINST THE MISSION AS THEY ARE BURNT AND SABOTAGING VIA VOTE".format(self.player_number))
                 return False
 
             return voting.spy_vote(self.missions_failed,
@@ -168,18 +160,10 @@ class DeterministicAgent(Agent):
 
         mission_go_ahead = len(votes) >= self.number_of_players / 2
 
-        if self.player_number == 0:
-            logging.debug("MISSION GO AHEAD: {} | {}".format(mission_go_ahead, mission))
-
         # Anyone that votes for burnt agents is a spy
         if any([agent for agent in mission if agent in self._get_burnt_spies()]):
-
             self.agent_assessments[proposer].distrust_level = 1.0
             self.agent_assessments[proposer].burnt = True
-
-            logging.debug("BURNT AGENT {} FOUND IN MISSION {} PROPOSED BY {}".format(str(mission),
-                                                                                     str(self._get_burnt_spies()),
-                                                                                     proposer))
 
     def _get_burnt_spies(self):
         '''A burnt spy is one that has been on a mission where the number of betrayals
@@ -206,7 +190,6 @@ class DeterministicAgent(Agent):
 
         # Ensure that resistance don't accidentally betray the mission
         if not self.is_spy():
-            #logging.debug("NOT A SPY: ", self.player_number)
             return False
 
         # Setup betrayal space
@@ -215,33 +198,24 @@ class DeterministicAgent(Agent):
 
         # Do not betray mission that can't fail
         if len(spies_on_mission) < betrayals_required:
-            logging.debug("ROUND {} BETRAYAL UNACHIEVABLE AGENT {} LAYING LOW".format(self.current_round, self.player_number))
             return False
-
-        logging.debug("BETRAYALS REQUIRED: {} SPIES AVAILABLE: {} BETRAYAL ACHIEVABLE".format(betrayals_required,
-                                                                                                 len(spies_on_mission)))
 
         time_pressure = self.current_round / 4
 
-        # Always betray last round as zero risk
-        if time_pressure == 1:
-            logging.debug("LAST ROUND BETRAYAL: {}".format(self.player_number))
+        # Zero risk for betraying on last round or to win
+        if time_pressure == 1 or self.missions_failed == 2:
             return True
-        elif self.missions_failed == 2:
-            logging.debug("WINNING BETRAYAL: {}".format(self.player_number))
-            return True
-
+        
 
         # COLLUSION MODULE : WARNING PLAY OUTSIDE THE SPIRIT OF THE GAME USING
         #                    EX ANTE COLLUSION. MUS BE SWITCHED ON PRIOR TO THE
         #                    GAME BEING STARTED
         if self.collusion and len(spies_on_mission) > 1 and self.fails_required[self.number_of_players][self.current_round] == 1:
-
+            
+            # If player is 'max(spies_on_mission)' they will sabotage otherwise they won't
             if self.player_number == max(spies_on_mission):
-                logging.debug("COLLUSIVE BETRAYAL BY LEAD SPY ROUND: {} AGENT: {}".format(self.current_round, self.player_number))
                 return True
             else:
-                logging.debug("COLLUSIVE BETRAYAL BY LEAD SPY ({}) ROUND {}: AGENT {} LAYING LOW".format(max(spies_on_mission), self.current_round, self.player_number))
                 return False
 
         # RANDOM MODULE
@@ -253,35 +227,35 @@ class DeterministicAgent(Agent):
         if self.agent_assessments[self.player_number].burnt:
             return True
 
+        # Betray because agent is the only spy on the mission 
+        # and it is late in the game
         if len(spies_on_mission) == 1 and time_pressure >= 0.5:
-            logging.debug("BETRAYAL OF OPPORTUNITY ROUND: {} AGENT: {}".format(self.current_round, self.player_number))
             return True
 
         # Determine actions when multiple spies on mission
         if len(spies_on_mission) > 1:
 
+            # If it is an early round and a mission has already failed 
+            # we want to lay low to build trust
             if time_pressure <= 0.5 and self.missions_failed == 1:
-                logging.debug("ASSESSING RISK ROUND {}: AGENT {} LAYING LOW".format(max(spies_on_mission), self.current_round, self.player_number))
                 return False
+            
+            # If we need to risk it then we will use a probability
             elif len(spies_on_mission) == len(mission):
                 
                 if self.current_round <= 1: 
                     
-                    # TODO - Replace Manual Calc for single round option Nash Eq. Calculator
-                    # Based on number of rounds
-                    if random.random() > 0.85:
+                    # Increase likelihood of failing mission for subsequent rounds
+                    if random.random() > 0.85 * (self.current_round + 1):
                         return True
                     else:
                         return False
 
-        logging.debug("BETRAYAL OF OPPORTUNITY ROUND: {} AGENT: {}".format(self.current_round, self.player_number))
+        # When in doubt sabotage the mission
         return True
 
     def mission_outcome(self, mission, proposer, betrayals, mission_success):
         '''Update world understanding based on mission outcome'''
-
-        if self.player_number == 0:
-            logging.debug("MISSION SUCCESS: {}".format(mission_success))
 
         # If all agents betray the mission they have burned themselves
         if len(mission) == betrayals:
@@ -290,7 +264,6 @@ class DeterministicAgent(Agent):
                 self.agent_assessments[agent].distrust_level = 1.0
                 self.agent_assessments[agent].burnt = True
 
-            logging.debug("SPIES BURNED THEMSELVES in ROUND {} : {}".format(self.current_round, str(self._get_burnt_spies())))
             return
 
         # Update the agent depending on role
@@ -316,28 +289,18 @@ class DeterministicAgent(Agent):
             for agent in known_spies:
                 self.agent_assessments[agent].distrust_level = 1.0
 
-            logging.debug("AGENT(S) ({}) CONFIRMED AS SPIES in ROUND {} TO PLAYER: {}".format(str(known_spies), self.current_round, self.player_number))
-
     def round_outcome(self, rounds_complete, missions_failed):
         '''Update rounds and mission failures'''
         self.current_round = rounds_complete
         self.missions_failed = missions_failed
 
     def game_outcome(self, spies_win, spies):
-        '''This shouldn't matter to Reflex Agent'''
+        '''Provide feedback to testing functions'''
 
-        if self.player_number == 0:
-            logging.debug("SPIES WIN: {}  SPIES WERE: {}\n".format(spies_win, str(spies)))
-        
         if (self.is_spy() and spies_win) or (not self.is_spy() and not spies_win):
             self.winner = True
         else:
             self.winner = False
-
-
-
-
-
 
 
 class Vote():
@@ -361,27 +324,23 @@ class Vote():
 
         # Spies shouldn't vote for burnt assets
         if any([agent in self.mission for agent in self.agent_assessments if self.agent_assessments[agent].burnt]):
-            self._vote_logging("(SPY) VOTING AGAINST BURNT ASSET ")
             return False
 
         # We are trying to frame someone so we should vote against them
         no_targets = all([target in self.mission for target in target_resistance])
         if (len(target_resistance) != 0 and no_targets) or self.proposer in target_resistance :
-            self._vote_logging("(SPY) VOTING NO TO FRAME TARGET")
             return False
 
         # No spies in mission
         if all([team_member not in self.spies for team_member in self.mission]):
 
-            # If we have breathing room then allow it
+            # If we can still win without this mission then allow it
             if 4 - self.current_round >= 3 - missions_failed:
-                self._vote_logging("(SPY) VOTING YES AS {} ROUNDS LEFT AND ONLY {} FAILS REQUIRED".format(4 - self.current_round, 3 - missions_failed))
                 return True
             else:
-                self._vote_logging("(SPY) VOTING NO DUE TO NUMBER OF ROUNDS LEFT")
                 return False
 
-        self._vote_logging("(SPY) VOTING YES AS NO REASON NOT TO ")
+        # If no reason not to vote yes then allow it
         return True
 
     def resistance_vote(self):
@@ -393,23 +352,11 @@ class Vote():
 
         # Only vote against a player if we know they are a spy
         if any([agent in self.mission for agent in self.agent_assessments if self.agent_assessments[agent].distrust_level == 1.0]):
-            self._vote_logging("(RESISTANCE) VOTING AGAINST KNOWN SPY")
-
             return False
 
-        self._vote_logging("(RESISTANCE) VOTING YES AS NO INFORMATION NOT TO")
+        # If no reason not to vote yes then allow it
         return True
 
-    def _vote_logging(self, reason):
-
-        log_output = "ROUND: {} PROPOSER: {} VOTING PLAYER: {} MISSION: {} {} {}"
-        log_output = log_output.format(self.current_round,
-                                       self.proposer,
-                                       self.player_number,
-                                       str(self.mission),
-                                       reason,
-                                       str(self.confirmed_spies))
-        logging.debug(log_output)
 
 
 class TeamBuilder():
@@ -424,9 +371,8 @@ class TeamBuilder():
 
         team = []
 
-        # Without adding ourself the mission will fail
-        if (self.number_of_players - 1) - team_size < number_of_spies:
-            team.append(self.player_number)
+        # Add self to mission
+        team.append(self.player_number)
 
         while len(team) < team_size:
 
@@ -445,15 +391,10 @@ class TeamBuilder():
         team = []
         number_of_spies = len(spies)
 
-        # If agent is burnt add self to poison vote
-        if self.player_number in self.confirmed_spies:
-            team.append(self.player_number)
+        # Always add self
+        team.append(self.player_number)
 
-        # Without adding ourself the mission will succeed/fail
-        elif (self.number_of_players - 1) - team_size < number_of_spies:
-            team.append(self.player_number)
-
-        #If self not already in the team random spies until we have enough
+        #Add random spies until we have enough to kill the mission
         while len(team) < betrayals_required:
 
             if len(self.confirmed_spies) - number_of_spies < betrayals_required:
@@ -477,7 +418,3 @@ class TeamBuilder():
         random.shuffle(team)
         return team
 
-
-class BadProbabilityException(Exception):
-    '''Raise when an exception is calculated that does not fall
-    between 0.0 and 1.0 incluseively'''
